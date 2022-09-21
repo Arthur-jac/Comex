8490f9fd4e9f5197d42a5f1ba10e8fd7316dde37
Arthur-jac <arthurjacomite@hotmail.com>
2022-09-21 11:16:04 -0300

    Criação da classe Cliente para o projeto remoto

---
+++ Comex/src/Cliente.java
@@ -0,0 +1,77 @@
+
+public class Cliente {
+	private int id;
+	private String nome;
+	private int cpf;
+	private int telefone;
+	private String rua;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	
+	
+	
+	public Cliente(int id, String nome, int cpf, int telefone, String rua, String numero, String complemento,
+			String bairro, String cidade, String estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.rua = rua;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+
+	@Override
+	public String toString() {
+		return " \n Cliente ID ->" + id + " \n"
+				+ " Nome -> " + nome + " \n"
+				+ " CPF -> " + cpf + " \n"
+				+ " Telefone -> " + telefone + " \n"
+				+ " Rua -> " + rua
+				+ " Numero ->" + numero + "\n"
+				+ " Complemento -> " + complemento + "\n"
+				+ " Bairro -> " + bairro + " \n"
+				+ " Cidade -> " + cidade + " \n"
+				+ " Estado -> " + estado + " \n \n";
+	}
+	
+	
+}
---
+++ Comex/src/MainCliente.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class MainCliente {
+
+	public static void main(String[] args) {
+		ArrayList<Object> clientes = new ArrayList<>();
+		
+		Cliente c1 = new Cliente(1, "Ana", 123, 999, "Rua da Ana", "1111", "Sobrado 1", "Bairro da Ana", "Curitiba", "PR");
+		
+		Cliente c2 = new Cliente(2, "Eli", 456, 888, "Rua da Eli", "2222", "Sobrado 2", "Bairro da Eli", "São Paulo", "SP");
+		
+		Cliente c3 = new Cliente(3, "Gabi", 789, 777, "Rua da Gabi", "3333", "Sobrado 3", "Bairro da Gabi", "Rio de Janeiro", "RJ");
+		
+		clientes.add(c1);
+		clientes.add(c2);
+		clientes.add(c3);
+
+		System.out.println(clientes);
+
+	}
+
+}
