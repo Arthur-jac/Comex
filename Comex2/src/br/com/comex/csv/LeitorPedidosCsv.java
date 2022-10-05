8fdc44b74fa96fa6531f4b1c27f7a972bbcc09e2
Arthur-jac <arthurjacomite@hotmail.com>
2022-10-05 09:06:55 -0300

    Lendo o arquivo pedidos.csv

---
+++ Comex2/src/br/com/comex/csv/LeitorPedidosCsv.java
@@ -0,0 +1,35 @@
+package br.com.comex.csv;
+
+import java.util.ArrayList;
+
+public class LeitorPedidosCsv {
+	
+	public static PedidoCsv cadastraPedido(String categoria, String produto, String cliente, String preco, String quantidade, String data) {
+		PedidoCsv pc = new PedidoCsv(categoria, produto, cliente, preco, quantidade, data);
+		return pc;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<PedidoCsv> pedidos = new ArrayList<PedidoCsv>();
+		
+		pedidos.add(cadastraPedido("MÓVEIS","Sofá 3 lugares","ANA","2500.00","1","05/01/2022"));
+		pedidos.add(cadastraPedido("INFORMÁTICA","Notebook Samsung","ANA","3523.00","1","01/01/2022"));
+		pedidos.add(cadastraPedido("LIVROS","Clean Architecture","ANA","102.90","2","08/01/2022"));
+		pedidos.add(cadastraPedido("MÓVEIS","Mesa de jantar 6 lugares","ELI","3678.98","1","06/01/2022"));
+		pedidos.add(cadastraPedido("CELULARES","iPhone 13","ANA","1889.00","6","13/01/2022"));
+		pedidos.add(cadastraPedido("INFORMÁTICA","Monitor Dell 27","DANI","1889.00","3","04/01/2022"));
+		pedidos.add(cadastraPedido("LIVROS","Implementing Domain-Driven Design","GABI","144.07","3","10/01/2022"));
+		pedidos.add(cadastraPedido("AUTOMOTIVA","Jogo de pneus","BIA","1276.79","1","15/01/2022"));
+		pedidos.add(cadastraPedido("LIVROS","Clean Code","BIA","95.17","1","09/01/2022"));
+		pedidos.add(cadastraPedido("CELULARES","Galaxy S22 Ultra","DANI","8549.10","5","14/01/2022"));
+		pedidos.add(cadastraPedido("INFORMÁTICA","Macbook Pro 16","CAIO","31752.00","1","03/01/2022"));
+		pedidos.add(cadastraPedido("LIVROS","Refactoring Improving the Design of Existing Code","DANI","173.90","1","12/01/2022"));
+		pedidos.add(cadastraPedido("MÓVEIS","Cama queen size","DANI","3100.00","2","07/01/2022"));
+		pedidos.add(cadastraPedido("AUTOMOTIVA","Central multimidia","CAIO","711.18","1","16/01/2022"));
+		pedidos.add(cadastraPedido("LIVROS","Building Microservices","CAIO","300.28","2","11/01/2022"));
+		pedidos.add(cadastraPedido("INFORMÁTICA","Galaxy Tab S8","BIA","5939.10","4","02/01/2022"));
+		
+		System.out.println("Total de pedidos -> " + pedidos.size());
+	}
+}
+
