4379f9385e98216d5411e6afa81ff06804c9be3f
Arthur-jac <arthurjacomite@hotmail.com>
2022-10-07 12:00:39 -0300

    Lógica melhorada no TotalCategoria

--- Comex2/src/br/com/comex/csv/TotalCategorias.java
+++ Comex2/src/br/com/comex/csv/TotalCategorias.java
@@ -2,46 +2,22 @@ package br.com.comex.csv;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TotalCategorias {
 	public static void main(String[] args) throws FileNotFoundException {
 		LeitorPedidosCsv lpc = new LeitorPedidosCsv();
 		ArrayList<PedidoCsv> pedidos = lpc.lerCsv();
 
-		int contador = 0;
-		boolean livros = true;
-		boolean informatica = true;
-		boolean celulares = true;
-		boolean automotiva = true;
-		boolean moveis = true;
+		
+		Set<String> set = new HashSet<String>(); 
 		
 		for (PedidoCsv p : pedidos) {
-			if (p.getCategoria().equals("LIVROS") && livros) {
-				contador++;
-				livros = false;
-			}
-			
-			if (p.getCategoria().equals("CELULARES") && celulares) {
-				contador++;
-				celulares = false;
-			}
-			
-			if (p.getCategoria().equals("INFORMÁTICA") && informatica) {
-				contador++;
-				informatica = false;
-			}
-			
-			if (p.getCategoria().equals("AUTOMOTIVA") && automotiva) {
-				contador++;
-				automotiva = false;
-			}
-			
-			if (p.getCategoria().equals("MÓVEIS") && moveis) {
-				contador++;
-				moveis = false;
-			}
+			set.add(p.getCategoria());
 		}
 		
-		System.out.println("Total de categorias -> " + contador);
+		
+		System.out.println("Total de categorias -> " + set.size());
 	}
 }
