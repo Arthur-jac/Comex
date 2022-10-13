b3cbee49369d0c8c6efa0c1762a472ae34a01b86
Arthur-jac <arthurjacomite@hotmail.com>
2022-10-13 14:23:03 -0300

    Atualização de Categorias

---
+++ Comex_BD/src/br/com/comex/main/MainAtualizacaoCategoria.java
@@ -0,0 +1,26 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MainAtualizacaoCategoria {
+
+	public static void main(String[] args) throws SQLException {
+Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","comex","comex");
+		
+		String comandoSql = "UPDATE comex.categoria SET nome=? WHERE id=?";
+		
+		PreparedStatement stm = con.prepareStatement(comandoSql); 
+	
+		stm.setString(1, "LIVROS TÉCNICOS");
+		stm.setInt(2, 11);
+		
+		stm.execute();
+		stm.close();
+		con.close();
+
+	}
+
+}
