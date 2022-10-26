eb4f75e556fcc5516e18e1f784b392f18fba14c8
Arthur-jac <arthurjacomite@hotmail.com>
2022-10-26 14:37:31 -0300

    Volumes no container do Oracle

--- oracle.sh
+++ oracle.sh
@@ -1,2 +1,6 @@
-docker run -d --name oraclebd -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
-docker container logs -f oraclebd
+docker run -d --name oracle -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker container logs -f oracle
+
+docker rm adcfebd78b33 --force
+docker create volume oracle-volume
+docker run -d -v oracle-volume:/opt/oracle/oradate oracle -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
